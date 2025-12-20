d8ee39ff5c3dd969cf19a66ea0491c22323f4661
BertrandOscarSaputra <bertrandosc@gmail.com>
2025-12-21 04:50:43 +0800

    hsts add

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,10 @@ const securityHeaders = [
       .trim(),
   },
   {
+    key: "Strict-Transport-Security",
+    value: "max-age=86400",
+  },
+  {
     key: "X-Content-Type-Options",
     value: "nosniff",
   },
@@ -39,6 +43,11 @@ const securityHeaders = [
     key: "Permissions-Policy",
     value: "camera=(), microphone=(), geolocation=()",
   },
+  {
+    key: "Cross-Origin-Opener-Policy",
+    value: "same-origin-allow-popups",
+  },
+  
 ];
 
 
